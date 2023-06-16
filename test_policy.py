fe9f33871784031e143bb4311d96f04d53444bb1
zhaoming <zhaoming@helsinki.stanford.edu>
2023-06-16 10:54:20 -0700

    update test policy so that we can test pretrain model

--- test_policy.py
+++ test_policy.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
    import json
    from ruamel.yaml import YAML, dump, RoundTripDumper
-   from raisimGymTorch.env.bin import motion_matching
+   from raisimGymTorch.env.bin import box_manipulation_release
    from raisimGymTorch.env.RaisimGymVecEnv import RaisimGymVecTorchEnv as VecEnv
    from raisimGymTorch.helper.raisim_gym_helper import ConfigurationSaver, load_param, tensorboard_launcher
 
@@ -19,25 +19,6 @@ if __name__ == '__main__':
    import os
    import numpy as np
    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-   #load mocap and build KD tree
-   # from process_mocap import process_mocap
-   # from sklearn.neighbors import KDTree
-   # mocap = load_mocap()
-   # feature_mean = np.mean(mocap["features"], axis=0)
-   # feature_std = np.std(mocap["features"], axis=0)
-   # feature_std[2:4] /= 2
-   # feature_std[4:6] /= 2
-   # for i in range(feature_std.shape[0]):
-   #    if abs(feature_std[i]) < 0.00001:
-   #       feature_std[i] = 1
-   # features = (features - feature_mean) / feature_std
-   # tree = KDTree(features)
-
-   with open('reference.npy', 'rb') as f:
-      reference = np.load(f)
-      reference[:, 2] += 0.05
-   reference = np.repeat(reference[np.newaxis, :, :], 2, axis=0).astype(np.float32)
  
    seed = 3#8
    torch.manual_seed(seed)
@@ -52,23 +33,13 @@ if __name__ == '__main__':
    cfg = YAML().load(open(task_path + "/cfg.yaml", 'r'))
 
    # create environment from the configuration file
-   env = VecEnv(motion_matching.RaisimGymEnv(home_path + "/rsc", dump(cfg['environment'], Dumper=RoundTripDumper)), cfg['environment'])
+   env = VecEnv(box_manipulation_release.RaisimGymEnv(home_path + "/rsc", dump(cfg['environment'], Dumper=RoundTripDumper)), cfg['environment'])
    num_envs = 1
    kd_tree_env = KD_tree_Env(num_envs)
    print("env_created")
 
-   num_inputs = 84#env.observation_space.shape[0]
-   num_outputs = 9#env.action_space.shape[0] + 6
-   model = ActorCriticNet(num_inputs, num_outputs, [128, 128])
-   # model.load_state_dict(torch.load("stats/dynamics/iter7999.pt")) #2999 for speed 3
-   model.cuda()
-
    dynamic_model = ActorCriticNetMann(env.observation_space.shape[0], env.action_space.shape[0], [128, 128])
-   # dynamic_model.load_state_dict(torch.load("stats/carry_policy/iter25999.pt"))
-   dynamic_model.load_state_dict(torch.load("stats/weak_30Hz/iter23999.pt"))
-   # dynamic_model.load_state_dict(torch.load("pick_policy_Jan05.pt"))
-   # dynamic_model.load_state_dict(torch.load("stats/dynamics_ELU/iter113999.pt")) #train with runing and pick up
-   # dynamic_model.load_state_dict(torch.load("stats/dynamics_ELU_long/iter62999.pt")) #train with runing and pick up
+   dynamic_model.load_state_dict(torch.load("pick_and_place_model.pt"))
    dynamic_model.cuda()
 
    # env.setTask()
@@ -90,9 +61,6 @@ if __name__ == '__main__':
    torque_profile = np.zeros(10000)
    while True:
       counter = 0
-      with torch.no_grad():
-         act = model.sample_best_actions(kin_state)
-
       dynamic_done = np.zeros(num_envs, dtype=np.int)
       for t in range(5):
          kd_tree_env.current_phase = kd_tree_env.phase[ind, t].copy()
@@ -139,10 +107,6 @@ if __name__ == '__main__':
             state_counter = 0
             obs = env.observe()
       # print(obs)
-
-      if use_kinematics:
-         env.set_reference(np.concatenate((kd_tree_env.current_phase, kd_tree_env.reference), axis=1).astype(np.float32))
-         obs, rew, done, _ = env.step(torch.zeros(num_envs, env.action_space.shape[0], device=device))
       kin_state = env.get_reference()
       if dynamic_done[0] > 0 or state_counter >= 720:
          import ipdb; ipdb.set_trace()
@@ -152,10 +116,7 @@ if __name__ == '__main__':
          env.set_reference_velocity(kd_tree_env.reference_velocity.astype(np.float32))
          env.reset()
          obs = env.observe()
-         # import ipdb; ipdb.set_trace()
-         # obs[:, :-6] += torch.randn(num_envs, num_inputs - 6, device=device) * 0.1
-      import time; time.sleep(0.001)
+
+      import time; time.sleep(0.032)
 
       print(ind[0], rew[0], done[0])
-
-      import time; time.sleep(0.0)
